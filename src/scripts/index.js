df8e2b73be886959f68bf2d56630aad3fcd70a20
Alex Pirko <youzhny_foto@gmail.com>
2023-04-26 11:53:00 +0300

    feat:bind keyboard and textarea

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,7 +9,7 @@ class Keyboard {
     this.capsLock = false;
   }
 
-  createHtmlElements() {
+  init() {
     const main = document.createElement('main');
     const keyboardContainer = document.createElement('div');
     const title = document.createElement('h1');
@@ -27,9 +27,12 @@ class Keyboard {
     systemInfo.classList.add('subtitle');
     keyboardContainer.appendChild(textarea);
     textarea.classList.add('textarea');
+    textarea.setAttribute('id', 'textarea');
     keyboardContainer.appendChild(keysContainer);
     keysContainer.classList.add('keyboard');
     keysContainer.appendChild(this.createKeyElements());
+
+    this.keys = keysContainer.querySelectorAll('.keybtn__letter');
 
     title.textContent = 'Virtual keyboard by Alex Pirko, RSS';
     systemInfo.textContent = 'The keyboard was created on Windows. To change the language press Ctrl + Alt';
@@ -37,7 +40,7 @@ class Keyboard {
 
   createKeyElements() {
     const fragment = document.createDocumentFragment();
-    const keyLayout = [
+    const engKeyLayout = [
       '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
       'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'delete',
       'capsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'enter',
@@ -45,9 +48,18 @@ class Keyboard {
       'ctrl', 'win', 'alt', 'space', 'alt', '◄', '▼', '►', 'ctrl',
     ];
 
+    const ruKeyLayout = [
+      'ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
+      'tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'delete',
+      'capsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'enter',
+      'shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'shift',
+      'ctrl', 'win', 'alt', 'space', 'alt', '◄', '▼', '►', 'ctrl',
+    ];
+
+    const keyLayout = engKeyLayout;
+
     keyLayout.forEach((key) => {
       const keyEl = document.createElement('button');
-      const lineBreak = ['backspace', 'delete', 'enter', 'shift'].indexOf(key) !== -1;
 
       keyEl.classList.add('keybtn');
       keyEl.setAttribute('type', 'button');
@@ -58,7 +70,9 @@ class Keyboard {
           keyEl.textContent = 'Backspace';
 
           keyEl.addEventListener('click', () => {
-            this.properties.value = this.propeprties.value.substring(0, this.properties.value.length - 1);
+            this.value = this.value
+              .substring(0, this.value.length - 1);
+            document.getElementById('textarea').innerHTML = this.value;
           });
 
           break;
@@ -68,7 +82,8 @@ class Keyboard {
           keyEl.innerHTML = 'Tab';
 
           keyEl.addEventListener('click', () => {
-            this.properties.value += '\t';
+            this.value += '\t';
+            document.getElementById('textarea').innerHTML = this.value;
           });
 
           break;
@@ -78,7 +93,9 @@ class Keyboard {
           keyEl.innerHTML = 'Del';
 
           keyEl.addEventListener('click', () => {
-            this.properties.value = this.propeprties.value.substring(0, this.properties.value.length + 1);
+            this.value = this.value
+              .substring(0, this.value.length - 1);
+            document.getElementById('textarea').innerHTML = this.value;
           });
 
           break;
@@ -88,7 +105,7 @@ class Keyboard {
           keyEl.innerHTML = 'CapsLock';
 
           keyEl.addEventListener('click', () => {
-            this._toggleCapsLock();
+            this.toggleCaps();
           });
 
           break;
@@ -98,7 +115,8 @@ class Keyboard {
           keyEl.innerHTML = 'Enter';
 
           keyEl.addEventListener('click', () => {
-            this.properties.value += '\n';
+            this.value += '\n';
+            document.getElementById('textarea').innerHTML = this.value;
           });
 
           break;
@@ -107,17 +125,14 @@ class Keyboard {
           keyEl.classList.add('keybtn__wide');
           keyEl.innerHTML = 'Shift';
 
-          keyEl.addEventListener('click', () => {
-
-          });
-
           break;
 
         case 'space':
           keyEl.classList.add('keybtn__superwide');
 
           keyEl.addEventListener('click', () => {
-            this.properties.value += '';
+            this.value += ' ';
+            document.getElementById('textarea').innerHTML = this.value;
           });
 
           break;
@@ -142,21 +157,61 @@ class Keyboard {
 
         default:
           keyEl.textContent = key.toLowerCase();
+          keyEl.classList.add('keybtn__letter');
 
           keyEl.addEventListener('click', () => {
-            this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+            this.value += this.capsLock
+              ? key.toUpperCase()
+              : key.toLowerCase();
+            // let initialValue = document.getElementById('textarea').innerHTML;
+            document.getElementById('textarea').innerHTML = this.value;
           });
       }
-
       fragment.appendChild(keyEl);
-
     });
 
     return fragment;
   }
+
+  toggleCaps() {
+    this.capsLock = !this.capsLock;
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key of this.keys) {
+      if (key.childElementCount === 0) {
+        key.textContent = this.capsLock
+          ? key.textContent.toUpperCase()
+          : key.textContent.toLowerCase();
+      }
+    }
+  }
+
+  // markEvent() {
+  //   document.addEventListener('keydown', function(e) {
+  //     const keys = document.querySelectorAll('.keybtn')
+  //     for(let i = 0; i < keys.length; i++) {
+  //         if(e.key == keys[i].getAttribute('keyname' ) || e.key == keys[i].getAttribute('lowerCaseName')) {
+  //             keys[i].classList.add('active')
+  //         }
+  //         if(e.code == 'Space') {
+  //             spaceKey.classList.add('active')
+  //         }
+  //         if(e.code == 'ShiftLeft') {
+  //             shift_right.classList.remove('active')
+  //         }
+  //         if(e.code == 'ShiftRight') {
+  //             shift_left.classList.remove('active')
+  //         }
+  //         if(e.code == 'CapsLock') {
+  //             caps_lock_key.classList.toggle('active');
+  //         }
+  //     }
+  //   })
+  // }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   const keyboard = new Keyboard();
-  keyboard.createHtmlElements();
+  keyboard.init();
+  console.log(keyboard);
 });
